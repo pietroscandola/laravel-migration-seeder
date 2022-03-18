151fabbae98ac0f5ae4c33155c20983b0ff58ab5
pietroscandola <pietro.scandolavr@gmail.com>
2022-03-18 19:06:58 +0100

    add cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,26 @@
 @section('title', 'Home')
 
 @section('content')
-    @foreach ($trains as $train)
-        {{$train->company}}
+    <div class="container mt-5">
+        <div class="d-flex">
+            @foreach ($trains as $train)
+                <div class="card me-5" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$train->company}}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Stazione di Partenza: {{$train->departure_station}}</h6>
+                        <h6 class="card-subtitle mb-2 text-muted">Stazione di Arrivo: {{$train->arrival_station}}</h6>
+                        <p class="card-text">Orario Partenza: {{$train->departure_time}}</p>
+                        <p class="card-text">Orario di Arrivo: {{$train->arrival_time}}</p>
+                        <p class="card-text">Numero Codice Treno: {{$train->train_code_number}}</p>
+                        <p class="card-text">Numero Carrozze: {{$train->carriages}}</p>              
+                    </div>
+                </div>
     
-    @endforeach
+            @endforeach
+
+        </div>
+        
+    </div>
+    
 
 @endsection
